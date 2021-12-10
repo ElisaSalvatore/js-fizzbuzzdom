f049a071f52e14e90d7b7d5d50f11d933c0a1e70
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-10 16:58:28 +0100

    add: boxes's style on Dom

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
 //creare la costante collegata all'html
-const numberList = document.querySelector('.number-list');
+/* const numberList = document.querySelector('.number-list'); */
+const containerBoxes = document.querySelector('.container-boxes');
 
-// stampare in console i numeri da 1 a 100
+// 1.stampare in console i numeri da 1 a 100
+// 2.creare un container nel DOM, a cui appendere un elemento html con il numero e la stringa corretta
+// 3.applicare uno stile CSS differente a seconda del valore dell’indice per i multipli di 3, 
+//   per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5
 
 for (let i = 1; i <= 100; i++) {
-   
     if (i % 3 === 0 && i % 5 === 0 ){    
         // multipli di 3 e di 5 "FizzBuzz" al posto del numero
-        numberList.innerHTML += 'fizzBuzz ';
+        containerBoxes.innerHTML += `<div class="box box-red"> fizzBuzz </div>`;
 
     } else if (i % 3 === 0){ 
         // multipli di 3 "Fizz" al posto del numero
-        numberList.innerHTML += 'fizz ';
+        containerBoxes.innerHTML += `<div class="box box-green"> fizz </div>`;
 
     } else if (i % 5 === 0) {                   
         // multipli di 5 "Buzz" al posto del numero
-        numberList.innerHTML += 'buzz ';
+        containerBoxes.innerHTML += `<div class="box box-yellow"> buzz </div>`;
 
     }  else {
-        numberList.innerHTML += `<span>${i}</span> `;
+        containerBoxes.innerHTML += `<div class="box">${i}</div>`;
     }
 }
 
 
-// creare un container nel DOM, a cui appendere unelemento html con il numero e la stringa corretta
-
-//applicare uno stile CSS differente a seconda del valore dell’indice per i multipli di 3, 
-//per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5
